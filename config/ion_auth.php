2b93ffc03e2560abf667fb81439db895e22a411b
Thomas Planer <tplaner@gmail.com>
2010-08-14 19:09:00 -0400

    Changed the default hash algorithm to sha1, set a default app_salt, changed the store_salt back to the default false, and modified the default password if you decide to change the value to true.

--- config/ion_auth.php
+++ config/ion_auth.php
@@ -85,7 +85,7 @@
 	 *
 	 * @author tplaner
 	 **/
-	$config['app_salt'] = 'FsSFKqCpFuAhs4ysuFQMkn2DzyJX3cF2b65CA4Z6eCc8M9nCmXFyt9GkcSeX5TBF';
+	$config['app_salt'] = 'Z6eCc8M9nCmXFyt9';
 	
 	/**
 	 * Hashing algorithm to use.
@@ -99,7 +99,7 @@
 	 *
 	 * @author tplaner
 	 **/
-	$config['hash_algorithm'] = 'sha512';
+	$config['hash_algorithm'] = 'sha1';
 
 	/**
 	 * Email Activation for registration
@@ -154,9 +154,9 @@
 	 * Should the salt be stored in the database?
 	 * This will change your password encryption algorithm, 
 	 * default password, 'password', changes to 
-	 * fbaa5e216d163a02ae630ab1a43372635dd374c0 with default salt.
+	 * 9efc31834d852ad365458342cc70dfad2744fee2 with default salt.
 	 **/
-	$config['store_salt'] = true;
+	$config['store_salt'] = false;
 	
 	/**
 	 * Message Start Delimiter
